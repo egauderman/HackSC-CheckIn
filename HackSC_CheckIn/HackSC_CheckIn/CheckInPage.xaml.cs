97ce984224f572fc1bc63d08d5f512907042ef12
Eric Gauderman <eric.gauderman@gmail.com>
2014-11-02 22:20:26 -0800

    Data binding for SearchResultsItemsControl works

--- HackSC_CheckIn/HackSC_CheckIn/CheckInPage.xaml.cs
+++ HackSC_CheckIn/HackSC_CheckIn/CheckInPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel; // for ObservableCollection<>
 using System.Linq;
 using System.Net;
 using System.Windows;
@@ -10,11 +11,31 @@ using Microsoft.Phone.Shell;
 
 namespace HackSC_CheckIn
 {
+	public class SearchResult
+	{
+		public string Name { get; set; }
+	}
+
 	public partial class CheckInPage : PhoneApplicationPage
 	{
+		public ObservableCollection<SearchResult> SearchResults = new ObservableCollection<SearchResult>();
+
 		public CheckInPage()
 		{
 			InitializeComponent();
+
+			//SearchResultsItemsControl.DataContext = SearchResults;
+
+			SearchResult s = new SearchResult();
+			s.Name = "User";
+			SearchResults.Add(s);
+
+			SearchResultsItemsControl.DataContext = SearchResults;
+		}
+
+		private void SearchQueryBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			
 		}
 	}
 }
