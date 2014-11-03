1de083d72e616fb2f2cd0aa2fd9556c9e4eb7e24
Eric Gauderman <eric.gauderman@gmail.com>
2014-11-02 16:58:27 -0800

    Added CheckInPage

--- HackSC_CheckIn/HackSC_CheckIn/CheckInPage.xaml.cs
+++ HackSC_CheckIn/HackSC_CheckIn/CheckInPage.xaml.cs
@@ -7,21 +7,14 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
-using HackSC_CheckIn.Resources;
 
 namespace HackSC_CheckIn
 {
-	public partial class MainPage : PhoneApplicationPage
+	public partial class CheckInPage : PhoneApplicationPage
 	{
-		// Constructor
-		public MainPage()
+		public CheckInPage()
 		{
 			InitializeComponent();
-		}
-
-		private void CheckInPageButton_Click(object sender, RoutedEventArgs e)
-		{
-			NavigationService.Navigate(new Uri("/CheckInPage.xaml", UriKind.Relative));
 		}
 	}
 }
