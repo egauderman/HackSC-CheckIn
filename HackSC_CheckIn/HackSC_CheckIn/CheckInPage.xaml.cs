ebfe1c85ddf75fd37f69c0360ebbfdeb056c253d
Eric Gauderman <eric.gauderman@gmail.com>
2014-11-02 23:45:51 -0800

    search now queries test server

--- HackSC_CheckIn/HackSC_CheckIn/CheckInPage.xaml.cs
+++ HackSC_CheckIn/HackSC_CheckIn/CheckInPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel; // for ObservableCollection<>
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Windows;
@@ -35,7 +36,39 @@ namespace HackSC_CheckIn
 
 		private void SearchQueryBox_TextChanged(object sender, TextChangedEventArgs e)
 		{
-			
+			// Query the server
+			string searchUri = "http://louise.codejoust.com/hacksc/people.php?q=" + SearchQueryBox.Text;
+
+			HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(searchUri);
+			request.BeginGetResponse(SearchQueryCallback, request);
+		}
+
+		private void SearchQueryCallback(IAsyncResult result)
+		{
+			HttpWebRequest request = result.AsyncState as HttpWebRequest;
+			if (request != null)
+			{
+				try
+				{
+					WebResponse response = request.EndGetResponse(result);
+					StreamReader reader = new StreamReader(response.GetResponseStream());
+					string searchResultJSON = reader.ReadToEnd();
+
+					Dispatcher.BeginInvoke(() =>
+					{
+						SearchResults.Add(new SearchResult { Name = searchResultJSON });
+					});
+				}
+				catch (WebException)
+				{
+					return;
+				}
+			}
+		}
+
+		private void SearchResultButton_Click(object sender, RoutedEventArgs e)
+		{
+
 		}
 	}
 }
